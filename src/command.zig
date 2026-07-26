65ec863cc6c9c7b4350039780a007c967e9996e5
Utku Özbek <uozbek29@outlook.com>
2026-07-26 12:18:34 +0300

    codecrafters submit [skip ci]

--- src/command.zig
+++ src/command.zig
@@ -402,12 +402,12 @@ fn handleXrange(io: Io, arena: std.mem.Allocator, w: *Io.Writer, store: *Store, 
 }
 
 fn handleXread(io: Io, arena: std.mem.Allocator, w: *Io.Writer, store: *Store, args: []const resp.Value) !void {
-    if (args.len != 3) return try resp.writeError(w, "ERR wrong number of arguments for 'xread'");
-    const key = switch (args[1]) {
+    if (args.len != 4) return try resp.writeError(w, "ERR wrong number of arguments for 'xread'");
+    const key = switch (args[2]) {
         .bulk_string => |m| m orelse return,
         else => return,
     };
-    const start_str = switch (args[2]) {
+    const start_str = switch (args[3]) {
         .bulk_string => |m| m orelse return,
         else => return,
     };
